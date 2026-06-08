218296797929c12525d4a5ef7ef5c5c0fac199c2
Hyeok.Lee <hlee1001@naver.com>
2026-06-08 14:12:12 +0900

    연습문제 2

---
+++ game/src/main/java/com/survivalcoding/Cleric.java
@@ -0,0 +1,24 @@
+package com.survivalcoding;
+
+public class Cleric {
+    public static final int MAX_HP = 50;
+    public static final int MAX_MP = 10;
+
+    String name;
+    int hp;
+    int mp;
+
+    void selfAid() {
+        if (mp >= 5) {
+            mp -= 5;
+            hp = MAX_HP;
+        }
+    }
+
+    public static void main(String[] args) {
+        Cleric myCleric = new Cleric();
+
+        myCleric.hp = 50;
+        myCleric.mp = 10;
+    }
+}
---
+++ game/src/main/java/com/survivalcoding/Main.java
@@ -0,0 +1,19 @@
+package com.survivalcoding;
+
+//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+public class Main {
+    public static void main(String[] args) {
+        // 인스턴스 생성
+        Hero hero = new Hero();
+
+
+        hero.hp = 10;
+        hero.attack();  // 9
+        hero.attack();  // 8
+
+
+        int a = 10;
+        long l = 10L; // literal 리터럴
+    }
+}
